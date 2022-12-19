6a9ddf4c7e50bd19aa305b0a3ca081a4700eca57
Justino Santos <santos@axiros.com>
2022-12-19 22:23:35 +0000

    day 6

---
+++ 6/6.1.py
@@ -0,0 +1,16 @@
+import sys 
+
+data = sys.stdin.read().strip()
+
+def all_unique(val):
+    return len(val) == len(set(val))
+
+for line in data.splitlines():
+    for i in range(len(line)):
+        window = line[i:i+4]
+        #print(f"[{i}] check [{window}]: {all_unique(window)} {len(window)} {len(set(window))} ")
+        
+        if all_unique(window):
+            print(i+4)
+            len(line)
+            break
